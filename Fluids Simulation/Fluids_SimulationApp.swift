3e87e1f6bccb0291056a08d67732fb7c06b7c8a0
Matthew Hartstein <baseballism14@icloud.com>
2025-10-24 17:10:08 -0500

    Initial Commit

---
+++ Fluids Simulation/Fluids_SimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fluids_SimulationApp.swift
+//  Fluids Simulation
+//
+//  Created by Matthew Hartstein on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Fluids_SimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fluids SimulationTests/Fluids_SimulationTests.swift
@@ -0,0 +1,16 @@
+//
+//  Fluids_SimulationTests.swift
+//  Fluids SimulationTests
+//
+//  Created by Matthew Hartstein on 10/24/25.
+//
+
+import Testing
+
+struct Fluids_SimulationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
